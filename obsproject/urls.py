f78b706a55beacbdeb63642b4c1594bca86f4a9b
Rishikesh-chaurasia <rishikeshchaurasiya516@gmail.com>
2026-05-28 09:27:36 +0530

    add superuser

--- obsproject/urls.py
+++ obsproject/urls.py
@@ -34,5 +34,7 @@ urlpatterns = [
     path('adminapp/', include('adminapp.adminappurls')),
     path('userapp/', include('userapp.userappurls')),
 ]
-urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
