4df21c035f662e426b37a79b70dc7bc097d6cead
Rishikesh-chaurasia <rishikeshchaurasiya516@gmail.com>
2026-05-30 23:35:13 +0530

    fix cloudinary media issue

--- obsproject/settings.py
+++ obsproject/settings.py
@@ -154,8 +154,8 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
