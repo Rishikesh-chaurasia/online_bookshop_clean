2882ef6741e0fa8df152620fe27e559d8282dffd
Rishikesh-chaurasia <rishikeshchaurasiya516@gmail.com>
2026-05-30 10:40:33 +0530

    fix db url

--- obsproject/settings.py
+++ obsproject/settings.py
@@ -83,11 +83,9 @@ WSGI_APPLICATION = 'obsproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
